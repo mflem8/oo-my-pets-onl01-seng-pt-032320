9fe3a1f339db0b0845d6e72634b0ba2c954eed81
Matt Fleming <matthewfleming8@gmail.com>
2020-04-24 01:03:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -40,4 +40,26 @@ class Owner
     self.cats.each {|cat| cat.mood = "happy"}
   end
 
-  
+  def sell_pets
+    self.cats.each do |cat|
+      self.cats.delete(cat)
+      cat.owner = nil
+      cat.mood = "nervous"
+    end
+    self.dogs.each do |dog|
+      self.dogs.delete(dog)
+      dog.owner = nil
+      dog.mood = "nervous"
+    end
+  end
+
+  def list_pets
+    number_of_dogs = self.dogs.count
+    number_of_cats = self.cats.count
+    "I have #{number_of_dogs} dog(s), and #{number_of_cats} cat(s)."
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+end

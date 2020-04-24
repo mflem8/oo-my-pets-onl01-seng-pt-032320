f5c766b4cf019ea70315a2c3cdc6f3a6600930d4
Matt Fleming <matthewfleming8@gmail.com>
2020-04-24 01:01:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,73 +1,43 @@
-require "pry"
-
 class Owner
-  @@all = []
-  attr_accessor :pets
+  attr_accessor :cats, :dogs
   attr_reader :species, :name
 
-  def initialize(species)
+  @@all = []
+
+  def initialize(name)
     @name = name
     @species = "human"
     @@all << self
-    @pets = {:dogs => [], :cats => []}
-  end
-
-  def say_species
-    return "I am a #{species}."
-  end
-
-  def buy_dog(name_of_dog)
-    @pets[:dogs] << Dog.new(name_of_dog)
-  end
-
-  def buy_cat(name_of_cat)
-    @pets[:cats] << Cat.new(name_of_cat)
-  end
-
-  def walk_dogs
-    @pets.collect do |species, instances|
-      if species == :dogs
-        instances.each do |dog|
-          dog.mood = "happy"
-        end
-      end
-    end
-  end
-
-  def feed_cats
-    @pets.collect do |species, instances|
-      if species == :cats
-        instances.each do |cat|
-          cat.mood = "happy"
-        end
-      end
-    end
-  end
-
-  def sell_pets
-    @pets.collect do |species, instances|
-      instances.each do |pet|
-        pet.mood = "nervous"
-      end
-      instances.clear
-    end
-  end
-
-  def list_pets
-    num_dogs = @pets[:dogs].size
-    num_cats = @pets[:cats].size
-    return "I have #{num_dogs} and #{num_cats}."
+    @cats = []
+    @dogs = []
   end
 
   def self.all
-    @@all
-  end
-
-  def self.reset_all
-    @@all.clear
+    @@all 
   end
 
   def self.count
-    @@all.size
+    self.all.length
   end
-end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+
+  
